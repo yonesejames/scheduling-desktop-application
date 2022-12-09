c874ddd7348cd69084c2a5a9e4acafa5836598dc
Yonese James <yoneseguerrier@gmail.com>
2022-12-08 21:54:33 -0500

    Added fxml variables from the fxml AddAppointmentView file to the java AddAppointmentController file

--- src/main/java/com/example/schedulingdesktopapplication/controller/AddAppointmentController.java
+++ src/main/java/com/example/schedulingdesktopapplication/controller/AddAppointmentController.java
@@ -3,6 +3,9 @@ package com.example.schedulingdesktopapplication.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,18 +17,82 @@ import java.util.ResourceBundle;
  */
 public class AddAppointmentController implements Initializable {
     /**
-     * Variables for the AddAppointmentController within the view files.
+     * FXML text field variable for the appointment's ID.
      */
     @FXML
     public TextField addAppointmentIDTextField;
+
+    /**
+     * FXML text field variable for the appointment's title.
+     */
     @FXML
     public TextField addAppointmentTitleTextField;
+
+    /**
+     * FXML text field variable for the appointment's type.
+     */
     @FXML
     public TextField addAppointmentTypeTextField;
+
+    /**
+     * FXML text field variable for the appointment's description.
+     */
     @FXML
     public TextField addAppointmentDescriptionTextField;
+
+    /**
+     * FXML text field variable for the appointment's location.
+     */
     @FXML
     public TextField addAppointmentLocationTextField;
+
+    /**
+     * FXML choice box variable for the appointment's start time.
+     */
+    @FXML
+    public ChoiceBox addAppointmentStartTimeChoiceBox;
+
+    /**
+     * FXML choice box variable for the appointment's contact.
+     */
+    @FXML
+    public ChoiceBox addAppointmentContactChoiceBox;
+
+    /**
+     * FXML choice box variable for the appointment's user ID.
+     */
+    @FXML
+    public ChoiceBox addAppointmentUserIDChoiceBox;
+
+    /**
+     * FXML choice box variable for the appointment's customer ID.
+     */
+    @FXML
+    public ChoiceBox addAppointmentCustomerIDChoiceBox;
+
+    /**
+     * FXML choice box variable for the appointment's end time.
+     */
+    @FXML
+    public ChoiceBox addAppointmentEndTimeChoiceBox;
+
+    /**
+     * FXML date picker variable for the appointment's start date.
+     */
+    @FXML
+    public DatePicker addAppointmentStartDateDatePicker;
+
+    /**
+     * FXML button variable to save the appointment.
+     */
+    @FXML
+    public Button addAppointmentSaveButton;
+
+    /**
+     * FXML button variable to cancel the appointment.
+     */
+    @FXML
+    public Button addAppointmentCancelButton;
 
     /**
      * Initialize method for the AddAppointmentController to initialize the stage and items.
