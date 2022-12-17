eb4451cd2d2e11181ecd1754cc7759a4e0bd004c
Yonese James <yoneseguerrier@gmail.com>
2022-12-17 14:38:50 -0500

    Added fxml variables from the fxml ModifyCustomerView file to the Java ModifyCustomerController file

--- src/main/java/com/example/schedulingdesktopapplication/controller/ModifyCustomercontroller.java
+++ src/main/java/com/example/schedulingdesktopapplication/controller/ModifyCustomercontroller.java
@@ -3,6 +3,8 @@ package com.example.schedulingdesktopapplication.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,18 +16,58 @@ import java.util.ResourceBundle;
  */
 public class ModifyCustomercontroller implements Initializable {
     /**
-     * Variables for the ModifyCustomercontroller within the view files.
+     * FXML text field variable for the customer's ID.
      */
     @FXML
     public TextField modifyCustomerIDTextField;
+
+    /**
+     * FXML text field variable for the customer's name.
+     */
     @FXML
     public TextField modifyCustomerNameTextField;
+
+    /**
+     * FXML text field variable for the customer's address.
+     */
     @FXML
     public TextField modifyCustomerAddressTextField;
+
+    /**
+     * FXML text field variable for the customer's phone number.
+     */
     @FXML
     public TextField modifyCustomerPhoneNumberTextField;
+
+    /**
+     * FXML text field variable for the customer's postal code.
+     */
     @FXML
     public TextField modifyCustomerPostalCodeTextField;
+
+    /**
+     * FXML choice box variable for the customer's country.
+     */
+    @FXML
+    public ChoiceBox modifyCustomerCountryChoiceBox;
+
+    /**
+     * FXML choice box variable for the customer's state and province.
+     */
+    @FXML
+    public ChoiceBox modifyCustomerStateAndProvinceChoiceBox;
+
+    /**
+     *  FXML save button variable to save the customer.
+     */
+    @FXML
+    public Button modifyCustomerSaveButton;
+
+    /**
+     *  FXML cancel button variable to revert back to the main screen.
+     */
+    @FXML
+    public Button modifyCustomerCancelButton;
 
     /**
      * Initialize method for the ModifyCustomercontroller to initialize the stage and items.
@@ -52,5 +94,45 @@ public class ModifyCustomercontroller implements Initializable {
      * @param actionEvent
      */
     public void modifyCustomerCancelButtonAction(ActionEvent actionEvent) {
+    }
+
+    /**
+     * modifyCustomerIDTextFieldAction method for the customer's ID.
+     *
+     * @param actionEvent
+     */
+    public void modifyCustomerIDTextFieldAction(ActionEvent actionEvent) {
+    }
+
+    /**
+     * modifyCustomerNameTextFieldAction method for the customer's name.
+     *
+     * @param actionEvent
+     */
+    public void modifyCustomerNameTextFieldAction(ActionEvent actionEvent) {
+    }
+
+    /**
+     * modifyCustomerAddressTextFieldAction method for the customer's address.
+     *
+     * @param actionEvent
+     */
+    public void modifyCustomerAddressTextFieldAction(ActionEvent actionEvent) {
+    }
+
+    /**
+     * modifyCustomerPhoneNumberTextFieldAction method for the customer's phone number.
+     *
+     * @param actionEvent
+     */
+    public void modifyCustomerPhoneNumberTextFieldAction(ActionEvent actionEvent) {
+    }
+
+    /**
+     * modifyCustomerPostalCodeTextFieldAction method for the customer's postal code.
+     *
+     * @param actionEvent
+     */
+    public void modifyCustomerPostalCodeTextFieldAction(ActionEvent actionEvent) {
     }
 }
